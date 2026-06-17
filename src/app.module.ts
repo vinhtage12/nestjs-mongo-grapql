de2136088095bc618c7b2cd058208953aef66a31
Nguyễn Tuấn Vinh <vinhtage@Vinhtage.local>
2026-06-17 15:37:08 +0700

    do something

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TranslationModule } from './features/translation/module/translation.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://root:example@localhost:27017'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
     TranslationModule,
   ],
   controllers: [],
